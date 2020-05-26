888a610a3cef4142484f472bc74d50acb5062ac1
Ezra Marks <43857686+EzraMarks@users.noreply.github.com>
2020-05-25 23:23:39 -0400

    Tweak tree size and depth

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ function setup() { //TODO re=run setup when page size changes
     createCanvas(window.innerWidth, window.innerHeight);
     forest = new Forest();
 
-    tree1 = new Tree(width / 2, height - 5, 225, forest);
+    tree1 = new Tree(width / 2, height - 5, 220, forest);
     forest.addTree(tree1);
     frameRate(60);
 }
--- tree.js
+++ tree.js
@@ -11,7 +11,7 @@ function Tree(x, y, height, forest) {
     this.height = height;
     this.forest = forest;
     // The recursion depth for creating new branches.
-    this.depth = 3 + floor(height / 75);
+    this.depth = 3 + floor(height / 70);
     // The percentage of life the tree has left.
     this.liveliness = 1;
     // 2D array of branches; each row contains all branches of a given depth.
